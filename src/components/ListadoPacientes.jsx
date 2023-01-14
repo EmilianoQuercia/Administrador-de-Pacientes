ff2ee95ef82e132ef11ec0b75bb020ef9d3c1e89
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-01-13 22:10:43 -0300

    creacion de proyecto y implementacion de tailwindCss

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListadoPacientes = () => {
+    return (
+        <div className='w-1/2 lg:w-3/5'>
+            <h2>Listado de Pacientes</h2>
+        </div>
+    );
+}
+
+export default ListadoPacientes;
